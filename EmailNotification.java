7ffb24d13b7e02c6ef341bdeff56a45a1dd943b6
Anusha Kalyani Muttu <53428898+AnushaKalyaniMuttu@users.noreply.github.com>
2023-08-07 09:14:35 +0530

    Create EmailNotification.java

---
+++ EmailNotification.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class EmailNotification implements Notification {
+
+	@Override
+	public void notifyUser() {
+		System.out.println("Email Notification");
+		
+	}
+
+	
+	
+}
